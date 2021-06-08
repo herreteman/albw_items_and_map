055670fa8a83f9100ca11a3a5375df29f8e372cd
Roger Carol <rogercarolmateo@gmail.com>
2021-06-08 04:01:55 +0200

    Variants with their settings

--- scripts/load_presets.lua
+++ scripts/load_presets.lua
@@ -1,11 +1,27 @@
-default = Preset:create({
-    1, --y_button
-    0, --dark rooms
-    1, --bee vendor
-    1, --captain sword
-    0, --borrowed sword
-    1, --vanilla lamp
-    1, --first bracelet
-    0, --open
-    1  --barrier
-})
+if Tracker.ActiveVariantUID == "standard" then
+	default = Preset:create({
+        0, --y_button (0 = locked / 1 = enabled)
+        0, --dark rooms (0 = lamp / 1 = no lamp)
+        0, --bee vendor (0 = required / 1 = not req)
+        0, --captain sword (0 = normal / 1 = skip)
+        0, --borrowed sword (0 = normal / 1 = shuffle)
+        0, --vanilla lamp (0 = normal / 1 = shuffle)
+        0, --first bracelet (0 = normal / 1 = skip)
+        0, --open (0 = false / 1 = true)
+        0  --barrier (0 = normal / 1 = start)
+    })
+end
+
+if Tracker.ActiveVariantUID == "open" then
+	default = Preset:create({
+        1, --y_button (0 = locked / 1 = enabled)
+        0, --dark rooms (0 = lamp / 1 = no lamp)
+        0, --bee vendor (0 = required / 1 = not req)
+        1, --captain sword (0 = normal / 1 = skip)
+        1, --borrowed sword (0 = normal / 1 = shuffle)
+        1, --vanilla lamp (0 = normal / 1 = shuffle)
+        1, --first bracelet (0 = normal / 1 = skip)
+        1, --open (0 = false / 1 = true)
+        1  --barrier (0 = normal / 1 = start)
+    })
+end
